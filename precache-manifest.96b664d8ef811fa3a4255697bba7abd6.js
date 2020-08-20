7f8b5053bbb50cad12d7b2f351ee169531183577
Aboneu <Aboneu@Aboneu-Macmini.local>
2020-08-20 20:33:08 +0900

    Updates

---
+++ precache-manifest.96b664d8ef811fa3a4255697bba7abd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6793742eb240393632f41a2f7a2ccd9",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "9597eb902987c0c3a3c9",
+    "url": "/movie_app_2020/static/css/main.7986fcd8.chunk.css"
+  },
+  {
+    "revision": "ff2e04eb8bcfc372b1ad",
+    "url": "/movie_app_2020/static/js/2.eb1b9d21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.eb1b9d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9597eb902987c0c3a3c9",
+    "url": "/movie_app_2020/static/js/main.2f2b59e6.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
